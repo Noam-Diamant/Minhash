932dc1d2c0356127f08c78b3084f957464c58db9
Noam-Diamant <111567319+Noam-Diamant@users.noreply.github.com>
2024-07-18 18:09:45 +0300

    Update proj_fm_ram_moving_tb.sv

--- proj_fm_ram_moving_tb.sv
+++ proj_fm_ram_moving_tb.sv
@@ -3,12 +3,15 @@
 module tb_proj_fm_ram;
 
     // Parameters
-    localparam ENTRIES = 8;
+    localparam BUFFERS = 1;
+    localparam RAMS = 2;
+    localparam ENTRIES = 4;
+    localparam OFFSET = 8;
+    localparam ITERATIONS = RAMS * ENTRIES * OFFSET;
     localparam DATA_BITS = 8;
-    localparam ADDR_BITS = $clog2(ENTRIES);
+    localparam ADDR_BITS = $clog2(ITERATIONS);
 
     // Testbench signals
-    logic [ADDR_BITS-1:0] in_addr;
     logic in_we;
     logic [DATA_BITS-1:0] in_wdata;
     logic [DATA_BITS-1:0] out_rdata;
@@ -17,10 +20,12 @@ module tb_proj_fm_ram;
 
     // Instantiate the DUT (Device Under Test)
     proj_fm_ram #(
+        .BUFFERS(BUFFERS),
+        .RAMS(RAMS),
         .ENTRIES(ENTRIES),
+        .OFFSET(OFFSET),
         .DATA_BITS(DATA_BITS)
     ) dut (
-        .in_addr(in_addr),
         .in_we(in_we),
         .in_wdata(in_wdata),
         .out_rdata(out_rdata),
@@ -40,7 +45,6 @@ module tb_proj_fm_ram;
         $display("Reseting...");
         in_rst_n = 1'b0;  // Assert reset
         in_we = 1'b1;
-        in_addr = 0;
         in_wdata = 0;
         #20;  // Wait for 20 ns
 
@@ -49,7 +53,7 @@ module tb_proj_fm_ram;
 
         // Write data to the RAM
         $display("Starting writing...");
-        for (int i = 0; i < ENTRIES; i++) begin
+        for (int i = 0; i < ITERATIONS; i++) begin
             in_wdata = i ;  // Example data
             in_we = 1'b1;
             $display("writing 8'h%0h to address %0h...", in_wdata, dut.waddr);
@@ -60,7 +64,6 @@ module tb_proj_fm_ram;
         $display("Reseting...");
         in_rst_n = 1'b0;  // Assert reset
         in_we = 1'b0;
-        in_addr = 0;
         in_wdata = 0;
         #10;  // Wait for 10 ns
         in_rst_n = 1'b1;  // Deassert reset
@@ -68,8 +71,8 @@ module tb_proj_fm_ram;
         // Read data from the RAM
         $display("Started reading...");
         in_we = 1'b0;
-        for (int i = 0; i < ENTRIES; i++) begin
-            $display("Reading data in Address: %0d | Data: %0h", dut.raddr, dut.data[dut.raddr]);
+        for (int i = 0; i < ITERATIONS; i++) begin
+            $display("Reading data in Address: %0h | Data: %0h", dut.raddr, dut.data[dut.raddr]);
             #10;  // Wait for a clock cycle
         end
 
